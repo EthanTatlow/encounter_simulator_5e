9b61689d8bc6c619f69d27cbbeeaf89bbe2bd892
Ethan Tatlow <ethan.tatlow@gmail.com>
2023-08-12 13:28:04 +0200

    Refactor main to include number of rounds required

--- src/main.rs
+++ src/main.rs
@@ -12,32 +12,56 @@ fn main() {
     let weapon_type = WeaponType::BattleAxe;
     println!("Weapon: {:?}", weapon_type);
 
-    let mut group1wins = 0;
-    let repetitions = 10000;
+    let mut group1_wins = 0;
+    let mut nr_rounds_sum = 0;
+    let repetitions = 1000;
+
     for _ in 0..repetitions {
-        let mut group1 = [
-            Character::new(WeaponType::Shortsword, AbilityModifiers::default(), 14, 14),
-            Character::new(WeaponType::Scimitar, AbilityModifiers::default(), 14, 30),
-        ];
+        let mut group1 = get_group_1();
+        let mut group2 = get_group_2();
 
-        let mut group2 = [
-            Character::new(WeaponType::Shortsword, AbilityModifiers::default(), 14, 14),
-            Character::new(WeaponType::Scimitar, AbilityModifiers::default(), 14, 30),
-        ];
-
+        let mut nr_rounds = 0;
         loop {
+            nr_rounds += 1;
             round::run_round(&mut group1, &mut group2);
             if group1.iter().all(|c| c.is_dead()) {
                 break;
             }
             if group2.iter().all(|c| c.is_dead()) {
-                group1wins += 1;
+                group1_wins += 1;
                 break;
             }
         }
+        nr_rounds_sum += nr_rounds;
     }
+
     println!(
         "Group 1 won {} % of the time",
-        group1wins as f32 / repetitions as f32 * 100.0
+        group1_wins as f32 / repetitions as f32 * 100.0
     );
+    println!(
+        "Average number of rounds: {}",
+        nr_rounds_sum as f32 / repetitions as f32
+    );
+}
+
+fn get_group_1() -> Vec<Character> {
+    (0..10)
+        .into_iter()
+        .map(|_| {
+            Character::new(
+                WeaponType::Shortsword,
+                AbilityModifiers::new(4, 4, 0, 0, 0, 0),
+                14,
+                9,
+            )
+        })
+        .collect()
+}
+
+fn get_group_2() -> Vec<Character> {
+    (0..20)
+        .into_iter()
+        .map(|_| Character::new(WeaponType::Shortsword, AbilityModifiers::default(), 14, 9))
+        .collect()
 }
